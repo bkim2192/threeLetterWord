b5d7009c6c07bdc94c9099577b6aa63a7f10fbd9
Brandon Kim <bkim2192@jh-mba158-16.local>
2019-01-08 14:39:25 -0600

    Initial Commit

---
+++ threeLetterWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  threeLetterWord
+//
+//  Created by Brandon Kim on 1/8/19.
+//  Copyright © 2019 Brandon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
