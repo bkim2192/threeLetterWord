9fef343cc113b6eeddc827d03624e37c981bd0d8
Brandon Kim <bkim2192@jh-mba158-16.local>
2019-01-11 14:24:45 -0600

    SO far

--- threeLetterWord/ViewController.swift
+++ threeLetterWord/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var letterLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet var labels: [UILabel]!
+    
+    let letters:[String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var counter = 0
+    var currentLetter:String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // struct Person {var name:String}
+        // let firstPerson = Person(name: "Jasmine")
+        //print(firstPerson.name)
+        currentLetter = letters[counter]
+        letterLabel.text = currentLetter
     }
 
+    @IBAction func whenTapped(_ sender: UITapGestureRecognizer) {
+        let selectedPoint = sender.location(in: stackView)
+        //print(selectedPoint)
+        for label in labels {
+//inside "labels" there are 3 labels, so it puts in currentLetter in those labels.
+            //print(label)
+            if label.frame.contains(selectedPoint){
+                label.text = currentLetter
+                
+            }
+        }
+    }
+    
+    
 
 }
 
